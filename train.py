96c84600f22545b659e8379ffbb0eb710ab0af67
Daniel Mooney <daniel.mooney180@gmail.com>
2025-09-06 15:36:45 +1000

    regular yolo training

---
+++ train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+import yaml
+import sys
+
+def main() -> None:
+    train_cfg = sys.argv[1]
+    model = YOLO()
+    results = model.train(cfg=train_cfg)
+    print(results)
+
+if __name__ == "__main__":
+    main()
